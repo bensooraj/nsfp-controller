5d549dbba5f2a159d6477a11dae6c790d1755c21
Ben Sooraj <bensooraj@users.noreply.github.com>
2020-12-18 22:48:34 +0530

    listing nodes!

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
 	"time"
 
 	version "github.com/bensooraj/nsfp-controller/version"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -16,6 +18,8 @@ import (
 
 func main() {
 	log.Printf("nsfp-controller version: %s", version.VERSION)
+
+	ctx := context.Background()
 
 	kubeconfig := flag.String("kubeconfig", "", "kubeconfig file")
 	flag.Parse()
@@ -40,6 +44,16 @@ func main() {
 	}
 
 	k8sClientset := kubernetes.NewForConfigOrDie(restConfig)
+
+	// List all the nodes!
+	nodeList, err := k8sClientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Error listing the nodes: %v\n", err)
+		os.Exit(2)
+	}
+	for index, node := range nodeList.Items {
+		log.Printf("Node #%d: %s | %+v\n", index, node.Name, node.Labels)
+	}
 
 	// Informer
 	sharedInformers := informers.NewSharedInformerFactory(k8sClientset, 30*time.Second)
